fec9d6a87463804d10940296b65f9bed4cad24e2
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-22 13:20:23 +0200

    Node installation and testing section completed

---
+++ 06. Brush With Node.js/app.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.mkdir('Dogs', { recursive: true }, (err) => {
+    console.log('In the callback');
+    if (err) throw err
+});
+
+fs.mkdirSync('Cats');
+console.log('I came after the code above');
